38d4f136cb006164e132762f61f64fc9333e1fb6
ra312 <akylzhanov.r@gmail.com>
2021-05-24 03:45:36 +0600

    Update OddOccurencesInArray.py

--- OddOccurencesInArray.py
+++ OddOccurencesInArray.py
@@ -1,6 +1,47 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
+#  0.040 s
+# RUNTIME ERROR, tested program terminated with exit code 1
+# stderr:
 
+# Traceback (most recent call last):
+#   File "exec.py", line 129, in <module>
+#     main()
+#   File "exec.py", line 91, in main
+#     result = solution( A )
+#   File "/tmp/solution.py", line 12, in solution
+#     return [num for num, counted in hashmap.items() if counted==1].pop()
+# IndexError: pop from empty list
+
+
+#  stderr:
+
+# Traceback (most recent call last):
+#   File "exec.py", line 129, in <module>
+#     main()
+#   File "exec.py", line 91, in main
+#     result = solution( A )
+#   File "/tmp/solution.py", line 12, in solution
+#     return [num for num, counted in hashmap.items() if counted==1].pop()
+# IndexError: pop from empty list
+
+
+
+# stderr:
+
+# Traceback (most recent call last):
+#   File "exec.py", line 129, in <module>
+#     main()
+#   File "exec.py", line 91, in main
+#     result = solution( A )
+#   File "/tmp/solution.py", line 12, in solution
+#     return [num for num, counted in hashmap.items() if counted==1].pop()
+# IndexError: pop from empty list
+
+
+
+
+    
 def solution(A):
     # write your code in Python 3.6
     # print(A)
